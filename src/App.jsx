b52914cf2dc28c1c67ba0bfc90ee93c5ab82accc
laizu akram <laizuakram98@gmail.com>
2023-05-12 09:31:55 +0600

    setup tailwind css and daisyui library with react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h3 className='text-3xl text-green-500 font-bold'>This is Guideasy task</h3>
+      <button className="btn">Button</button>
+    </>
+  )
+}
+
+export default App
