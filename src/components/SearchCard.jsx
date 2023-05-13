fd2c88e7f1b93bd89c836b620a450ec7a2f83f53
laizu akram <laizuakram98@gmail.com>
2023-05-13 17:17:39 +0600

    setup and use react icons

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {IoIosArrowDown} from 'react-icons/io'
+import '../components/SearchCard.css'
 
 const SearchCard = () => {
   return (
@@ -6,32 +8,32 @@ const SearchCard = () => {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="w-full">
-          <div className="stats shadow">
+          <div className="stats shadow cursor-pointer">
             
             {/* from state */}
-            <div className="stat w-80 h-36 text-left">
+            <div className="stat w-80 h-36 text-left transition ease-in duration-400">
               <div className="stat-title">From</div>
               <div className="stat-value font-bold">Delhi</div>
               <div className="stat-desc">DEL,Delhi airport india</div>
             </div>
 
             {/* to state */}
-            <div className="stat w-80 h-36 text-left">
+            <div className="stat w-80 h-36 text-left transition ease-in duration-400">
               <div className="stat-title">To</div>
               <div className="stat-value font-bold">Bengaluru</div>
               <div className="stat-desc">BLR,Bengaluru internation airport...</div>
             </div>
 
               {/* Deparature */}
-            <div className="stat text-left">
-              <div className="stat-title">Deparature</div>
+            <div className="stat text-left transition ease-in duration-400">
+              <div className="stat-title">Deparature <IoIosArrowDown className='inline text-blue-400 text-xl font-bold'/></div>
               <div className="stat-value font-bold">13 <span className='text-2xl font-medium'>May'23</span></div>
               <div className="stat-desc">Saturday</div>
             </div>
 
              {/* Return */}
-            <div className="stat text-left">
-              <div className="stat-title">Return</div>
+            <div className="stat text-left transition ease-in duration-400">
+              <div className="stat-title">Return <IoIosArrowDown className='inline text-blue-400 text-xl font-bold'/></div>
               <p className='w-36 font-medium text-sm text-gray-400'>Tap to add a return date for bigger discount</p>
               {/* <div className="stat-value">1,200</div>
               <div className="stat-desc">↘︎ 90 (14%)</div> */}
@@ -39,7 +41,7 @@ const SearchCard = () => {
 
               {/* travellers and class */}
             <div className="stat text-left">
-              <div className="stat-title">Travellers & Class</div>
+              <div className="stat-title">Travellers & Class <IoIosArrowDown className='inline text-blue-400 text-xl font-bold'/></div>
               <div className="stat-value font-bold">1 <span className='text-2xl font-medium'>Traveller</span></div>
               <div className="stat-desc">Economy/Premium Economy</div>
             </div>
